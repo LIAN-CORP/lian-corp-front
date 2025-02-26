d872d226626bd7c2f9d1983411fb0146ae07026e
Fsxerath <frediangarita1999@gmail.com>
2025-02-26 12:44:45 -0500

    add atoms file

--- src/app/components/atoms/save-button/save-button.component.ts
+++ src/app/components/atoms/save-button/save-button.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-save-button',
   standalone: true,
   imports: [],
   templateUrl: './save-button.component.html',
-  styleUrl: './save-button.component.scss'
+  styleUrl: './save-button.component.scss',
 })
 export class SaveButtonComponent {
-
+  @Input() text!: string;
 }
