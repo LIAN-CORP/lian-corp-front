31ed0b3060c4c1e9b0719364a85a0f0e208cf0ef
Fsxerath <frediangarita1999@gmail.com>
2025-02-26 12:06:13 -0500

    create buttons atoms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CancelButtonComponent } from "./components/cancel-button/cancel-button.component";
+import { DeleteButtonComponent } from "./components/delete-button/delete-button.component";
+import { SvgButtonComponent } from "./components/svg-button/svg-button.component";
+import { SaveButtonComponent } from "./components/save-button/save-button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CancelButtonComponent, DeleteButtonComponent, SvgButtonComponent, SaveButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/svg-button/svg-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './svg-button.component.html',
+  styleUrl: './svg-button.component.scss'
+})
+export class SvgButtonComponent {
+
+}
