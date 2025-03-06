ea1289037d3cee162d78d6ef8f03fc3dd3edcce0
Fsxerath <frediangarita1999@gmail.com>
2025-03-06 11:47:49 -0500

    feat (form-field): add logic in input control

--- src/app/components/molecules/form-field/form-field.component.ts
+++ src/app/components/molecules/form-field/form-field.component.ts
@@ -1,17 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { ErrorsDisplayComponent } from '../errors-display/errors-display.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-field',
   standalone: true,
-  imports: [ErrorsDisplayComponent],
+  imports: [ErrorsDisplayComponent, ReactiveFormsModule],
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.scss',
 })
 export class FormFieldComponent {
-  @Input() label!: string;
-  @Input() idInput!: string;
-  @Input() type!: string;
-  @Input() placeholder: string = '';
-  click = false;
+  label = input.required<string>();
+  idInput = input.required<string>();
+  type = input.required<string>();
+  placeholder = input<string>();
+  @Input({ required: true }) control!: FormControl;
+
+  getMessageError(): string {
+    const errors = this.control.errors;
+    if (errors?.['required']) {
+      return 'Este campo es requerido';
+    } else if (errors?.['minlength']) {
+      return `Debe tener al menos ${errors?.['minlength'].requiredLength} caracteres`;
+    } else if (errors?.['maxlength']) {
+      return `Debe tener como máximo ${errors?.['maxlength'].requiredLength} caracteres`;
+    } else if (errors?.['email']) {
+      return `Debe ser un correo electrónico válido`;
+    } else if (errors?.['passwordMatch']) {
+      return 'Las contraseñas no coinciden';
+    } else if (errors?.['pattern']) {
+      return 'Debe ser una URL válida';
+    }
+
+    return '';
+  }
 }
