ca0b58048dda44c5418345579fd5ce9b44c4f716
Fsxerath <frediangarita1999@gmail.com>
2025-03-25 12:36:36 -0500

    fix (menu): change styles and add open animation

--- src/app/components/organisms/menu/menu.component.ts
+++ src/app/components/organisms/menu/menu.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, input, output } from '@angular/core';
 import { SvgBgButtonComponent } from '../../atoms/svg-bg-button/svg-bg-button.component';
 import { SvgLinkDisplayComponent } from '../../molecules/svg-link-display/svg-link-display.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [SvgBgButtonComponent, SvgLinkDisplayComponent],
+  imports: [SvgBgButtonComponent, SvgLinkDisplayComponent, NgClass],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
